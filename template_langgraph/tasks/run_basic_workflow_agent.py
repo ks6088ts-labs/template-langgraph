ceded0201fd05f694ee0a230d5550fbe4d8c14c6
ks6088ts <ks6088ts@gmail.com>
2025-08-02 10:27:59 +0900

    add tool call agent

--- template_langgraph/tasks/run_basic_workflow_agent.py
+++ template_langgraph/tasks/run_basic_workflow_agent.py
@@ -1,27 +1,33 @@
 import logging
-import sys
 
-from template_langgraph.agents.basic_workflow_agent.agent import AgentInput, BasicWorkflowAgent
+from template_langgraph.agents.basic_workflow_agent.agent import AgentState
+from template_langgraph.agents.basic_workflow_agent.agent import graph as basic_workflow_agent_graph
 from template_langgraph.loggers import get_logger
 
 logger = get_logger(__name__)
 logger.setLevel(logging.INFO)
 
+
+def stream_graph_updates(
+    state: AgentState,
+) -> dict:
+    for event in basic_workflow_agent_graph.stream(input=state):
+        logger.info("-" * 20)
+        logger.info(f"Event: {event}")
+    return event
+
+
 if __name__ == "__main__":
-    question = "「鬼灯」を実行すると、KABUTOが急に停止します。原因と対策を教えてください。"
-    if len(sys.argv) > 1:
-        # sys.argv[1] が最初の引数
-        question = sys.argv[1]
-
-    # Agentのインスタンス化
-    agent = BasicWorkflowAgent()
-
-    # AgentInputの作成
-    agent_input = AgentInput(
-        request=question,
+    user_input = input("User: ")
+    state = AgentState(
+        messages=[
+            {
+                "role": "user",
+                "content": user_input,
+            }
+        ],
+        profile=None,
     )
-
-    # エージェントの実行
-    logger.info(f"Running BasicWorkflowAgent with input: {agent_input.model_dump_json(indent=2)}")
-    agent_output = agent.run_agent(input=agent_input)
-    logger.info(f"Agent output: {agent_output.model_dump_json(indent=2)}")
+    last_event = stream_graph_updates(state)
+    for value in last_event.values():
+        logger.info(f"Final state: {value['messages'][-1].content}")  # noqa: E501
