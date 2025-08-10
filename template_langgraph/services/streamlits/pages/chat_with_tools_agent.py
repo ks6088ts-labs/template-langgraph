54267ef3206a15483459403c12a5a46f0a42fd97
ks6088ts <ks6088ts@gmail.com>
2025-08-10 09:36:35 +0900

    add sql database tool

--- template_langgraph/services/streamlits/pages/chat_with_tools_agent.py
+++ template_langgraph/services/streamlits/pages/chat_with_tools_agent.py
@@ -5,22 +5,26 @@ from langchain_community.callbacks.streamlit import (
 
 from template_langgraph.agents.chat_with_tools_agent.agent import AgentState, graph
 
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
+
+for msg in st.session_state["chat_history"]:
+    if isinstance(msg, dict):
+        st.chat_message(msg["role"]).write(msg["content"])
+    else:
+        st.chat_message("assistant").write(msg.content)
+
 if prompt := st.chat_input():
+    st.session_state["chat_history"].append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     with st.chat_message("assistant"):
         response: AgentState = graph.invoke(
-            {
-                "messages": [
-                    {
-                        "role": "user",
-                        "content": prompt,
-                    },
-                ]
-            },
+            {"messages": st.session_state["chat_history"]},
             {
                 "callbacks": [
                     StreamlitCallbackHandler(st.container()),
                 ]
             },
         )
+        st.session_state["chat_history"].append(response["messages"][-1])
         st.write(response["messages"][-1].content)
--- template_langgraph/tools/common.py
+++ template_langgraph/tools/common.py
@@ -1,11 +1,15 @@
+from template_langgraph.llms.azure_openais import AzureOpenAiWrapper
 from template_langgraph.tools.cosmosdb_tool import search_cosmosdb
 from template_langgraph.tools.dify_tool import run_dify_workflow
 from template_langgraph.tools.elasticsearch_tool import search_elasticsearch
 from template_langgraph.tools.qdrant_tool import search_qdrant
+from template_langgraph.tools.sql_database_tool import SqlDatabaseClientWrapper
 
 DEFAULT_TOOLS = [
     search_cosmosdb,
     run_dify_workflow,
     search_qdrant,
     search_elasticsearch,
-]
+] + SqlDatabaseClientWrapper().get_tools(
+    llm=AzureOpenAiWrapper().chat_model,
+)
