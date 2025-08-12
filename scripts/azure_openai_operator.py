350d5c1c87d676f8c20ad09f2e01586234ef5e4d
ks6088ts <ks6088ts@gmail.com>
2025-08-12 14:50:13 +0900

    add analyze image command

--- scripts/azure_openai_operator.py
+++ scripts/azure_openai_operator.py
@@ -1,4 +1,5 @@
 import logging
+from base64 import b64encode
 
 import typer
 from dotenv import load_dotenv
@@ -14,6 +15,11 @@ app = typer.Typer(
 
 # Set up logging
 logger = get_logger(__name__)
+
+
+def load_image_to_base64(image_path: str) -> str:
+    with open(image_path, "rb") as image_file:
+        return b64encode(image_file.read()).decode("utf-8")
 
 
 @app.command()
@@ -70,6 +76,63 @@ def reasoning(
     logger.info("Running...")
     response = AzureOpenAiWrapper().reasoning_model.invoke(
         input=query,
+    )
+    logger.debug(
+        response.model_dump_json(
+            indent=2,
+            exclude_none=True,
+        )
+    )
+    logger.info(f"Output: {response.content}")
+
+
+@app.command()
+def image(
+    query: str = typer.Option(
+        "Please analyze the following image and answer the question",
+        "--query",
+        "-q",
+        help="Query to run with the Azure OpenAI chat model",
+    ),
+    file_path: str = typer.Option(
+        "./docs/images/streamlit.png",
+        "--file",
+        "-f",
+        help="Path to the image file to analyze",
+    ),
+    verbose: bool = typer.Option(
+        False,
+        "--verbose",
+        "-v",
+        help="Enable verbose output",
+    ),
+):
+    # Set up logging
+    if verbose:
+        logger.setLevel(logging.DEBUG)
+
+    base64_image = load_image_to_base64(file_path)
+    messages = {
+        "role": "user",
+        "content": [
+            {
+                "type": "text",
+                "text": query,
+            },
+            {
+                "type": "image",
+                "source_type": "base64",
+                "data": base64_image,
+                "mime_type": "image/png",
+            },
+        ],
+    }
+
+    logger.info("Running...")
+    response = AzureOpenAiWrapper().chat_model.invoke(
+        input=[
+            messages,
+        ],
     )
     logger.debug(
         response.model_dump_json(
